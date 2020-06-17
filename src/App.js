4fb3a1a1e8918743c9581f5337cfbf8355f6a8bd
kkimtae <kkimtae@gmail.com>
2020-06-17 16:52:13 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+function Movie(props) {
+  console.log(props);
+  return <h1>{props}</h1>;
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1>Hello!!</h1>
+      <Movie name="KIM" isMovie={true} />
+    </div>
+  );
 }
 
 export default App;
