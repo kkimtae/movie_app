3394cc133d359fa2add1b54e26a6e59d46c3d159
kkimtae <kkimtae@gmail.com>
2020-06-18 15:33:49 +0900

    state exmaple

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Movie(props) {
-  console.log(props.isMovie);
-  return (
-    <div>
-      <h1>i like {props.name}</h1>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <Movie name="kimchi" isMovie={true} />
-    </div>
-  );
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>current count is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
