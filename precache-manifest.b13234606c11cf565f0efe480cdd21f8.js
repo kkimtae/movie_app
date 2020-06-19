2f96185714518194bd16818df3fbee3f3d4c6d3c
kkimtae <kkimtae@gmail.com>
2020-06-19 15:55:20 +0900

    Updates

---
+++ precache-manifest.b13234606c11cf565f0efe480cdd21f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98751e7cb986cd33125efb4f5c73d1f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8a77242f0f0b4b7c6517",
+    "url": "/movie_app/static/css/main.6a52006b.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a77242f0f0b4b7c6517",
+    "url": "/movie_app/static/js/main.c40d8ecb.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
